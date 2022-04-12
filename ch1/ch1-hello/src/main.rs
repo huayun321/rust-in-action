7214f2da435031da4f54baad25792867ab723885
yun <huayun321@gmail.com>
2022-04-12 14:45:24 +0800

    add ch1-hello

---
+++ ch1/ch1-hello/src/main.rs
@@ -0,0 +1,15 @@
+fn greet_hello() {
+    let korean = "안녕하세요";
+    let english = "hello";
+    let chinese = "你好";
+
+    let regions = [korean, english, chinese];
+
+    for region in regions.iter() {
+        println!("{}", region);
+    }
+}
+
+fn main() {
+    greet_hello();
+}
