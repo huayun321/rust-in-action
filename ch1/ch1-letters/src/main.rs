47381fc7f99b4a408a771cea844286a38fa9088a
yun <huayun321@gmail.com>
2022-04-14 11:08:20 +0800

    add ch1-letters

---
+++ ch1/ch1-letters/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut letters = vec!['a', 'b', 'c'];
+
+    for letter in letters{
+        println!("{}", letter);
+
+        letters.push(letter.clone());
+    }
+}
+
+//试图在iterating的时候改变原数据
+
