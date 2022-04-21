7dabc4778bc1bb990b99b1472b5d480515d5aa35
yun <huayun321@gmail.com>
2022-04-21 18:56:56 +0800

    fix match needle

--- ch2/ch2-match-needles.rs
+++ ch2/ch2-match-needles.rs
@@ -4,7 +4,7 @@ fn main() {
 
     for item in &haystack {
         let result = match item { //This match expression returns a value that can be bound to a variable.
-            42 | 132 => "hit!", //Success! 42 | 132 matches both 42 and 132.
+            42 | 132 => "hit!", //Success! 42 | 132 matches  42 or 132.
             _ => "miss", //A wildcard pattern that matches everything
         };
 
