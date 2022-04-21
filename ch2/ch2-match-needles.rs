c0af9f07f193b3bc7c311c89b65308cee4b063da
yun <huayun321@gmail.com>
2022-04-21 18:47:18 +0800

    add match needle

---
+++ ch2/ch2-match-needles.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // let needle = 42;
+    let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862];
+
+    for item in &haystack {
+        let result = match item { //This match expression returns a value that can be bound to a variable.
+            42 | 132 => "hit!", //Success! 42 | 132 matches both 42 and 132.
+            _ => "miss", //A wildcard pattern that matches everything
+        };
+
+        if result == "hit!" {
+            println!("{}: {}", item, result);
+        }
+    }
+}
