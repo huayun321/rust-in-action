49c877abb1893772461cd8a09b43a26e8b9a71d4
yun <huayun321@gmail.com>
2022-04-21 16:40:35 +0800

    add ch2-add-floats

--- ch2/ch2-non-base2.rs
+++ ch2/ch2-non-base2.rs
@@ -1,7 +1,7 @@
 fn main() {
     let three = 0b11; // 2**1 * 1 + 2**0 * 1
-    let thirty = 0o36; // 8**1 * 3 + 8**1 * 6
-    let three_hundred = 0x12C; // 16**2 * 1 + 16**1 * 2 + 12
+    let thirty = 0o36; // 8**1 * 3 + 8**0 * 6
+    let three_hundred = 0x12C; // 16**2 * 1 + 16**1 * 2 + 16**0 * 12
 
     println!("base 10: {} {} {}", three, thirty, three_hundred);
     println!("base 2: {:b} {:b} {:b}", three, thirty, three_hundred);
